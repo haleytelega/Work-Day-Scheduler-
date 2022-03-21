8cccf32fa009bd8a0480a458be976329444172b5
Haley Telega <haleytelega@outlook.com>
2022-03-21 10:51:06 -0400

    got the save button to click and save

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,20 @@
     document.getElementById("currentDay").innerHTML=getCurrentDate;
 
 //making text box clickale
-// $(".task").text("this is working");
 $(".task").on("click", function() {
     var text = $(this).text().trim();
-    console.log("button is clicked");
+    console.log("text area has been clicked");
 
 var textInput = $("<textarea>").val(text);
     $(this).replaceWith(textInput);
 
+});
+
+//saving the textbox
+$(".saveBtn").on("click", function () {
+    $(this).text($(this).text() == 'Save');
+
+    $(".save").text("save");
+
+    console.log("this saved");
 });
