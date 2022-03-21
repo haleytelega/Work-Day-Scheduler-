05acb47ae51080f2de6019e0e810de1e586fab77
Haley Telega <haleytelega@outlook.com>
2022-03-21 10:15:54 -0400

    make first hour clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
     var getCurrentDate = moment().format("dddd, MMMM, Do");
     console.log(getCurrentDate);
     document.getElementById("currentDay").innerHTML=getCurrentDate;
+
+//making text box clickale
+// $(".task").text("this is working");
+$(".task").on("click", function() {
+    var text = $(this).text().trim();
+    console.log("button is clicked");
+
+var textInput = $("<textarea>").val(text);
+    $(this).replaceWith(textInput);
+
+});
